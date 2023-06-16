c0daf05f4099f957802a939939187e2b0b84f6fe
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-06-16 13:30:59 -0700

    s

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,46 +7,42 @@
  */
 int main(void)
 {
-	int i;
-	int j;
-	int k;
-	int l;
+        int i;
+        int j;
+        int k;
+        int l;
 
-	for (i = 48; i < 58; i++)
-	{
-		for (j = 48; j < 58; j++)
-		{
-			for (k = 48; k < 58; k++)
-			{
-				for (l = 48; l < 58; l++)
-				{
-					if (i == j && j == k && k == l)
-					{
-						continue;
-					}
+        for (i = 48; i < 58; i++)
+        {
+                for (j = 48; j < 58; j++)
+                {
+                        for (k = 48; k < 58; k++)
+                        {
+                                for (l = 48; l < 58; l++)
+                                {
+                                        if (i == j && j == k && k == l)
+                                        {
+                                                continue;
+                                        }
 					if (i == k && j == l)
 					{
 						continue;
 					}
-					if (i == k && j > l)
-					{
-						continue;
-					}
-					putchar(i);
-					putchar(j);
-					putchar(' ');
-					putchar(k);
-					putchar(l);
-					if (i == 57)
-					{
-						break;
-					}
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+                                        if (((j - 48) + ((i - 48) * 10)) >= ((l - 48) + ((k - 48) * 10)))
+                                        {
+                                                continue;
+                                        }
+                                        putchar(i);
+                                        putchar(j);
+                                        putchar(' ');
+                                        putchar(k);
+                                        putchar(l);
+                                        putchar(',');
+                                        putchar(' ');
+                                }
+                        }
+                }
+        }
+        putchar('\n');
+        return (0);
 }
---
+++ 0x01-variables_if_else_while/666.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int i;
+        int j;
+        int k;
+        int l;
+
+        i = (50 - 48) + ((56-48) * 10);
+        printf("%d",i);
+        return (0);
+}
