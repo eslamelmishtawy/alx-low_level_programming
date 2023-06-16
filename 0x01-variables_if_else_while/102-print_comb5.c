b00da0fbcc3312062ca687a10de7837719d13609
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-06-16 13:33:20 -0700

    s

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -37,6 +37,10 @@ int main(void)
                                         putchar(' ');
                                         putchar(k);
                                         putchar(l);
+					if(i == 57 && j == 57 && k == 57 && l == 57)
+					{
+						break
+					}
                                         putchar(',');
                                         putchar(' ');
                                 }
