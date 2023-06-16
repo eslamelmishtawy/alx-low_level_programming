e6350e660fb5e3e0b3c053289202d9a63c253117
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-06-16 13:46:53 -0700

    s

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,46 +7,46 @@
  */
 int main(void)
 {
-        int i;
-        int j;
-        int k;
-        int l;
+	int i;
+	int j;
+	int k;
+	int l;
 
-        for (i = 48; i < 58; i++)
-        {
-                for (j = 48; j < 58; j++)
-                {
-                        for (k = 48; k < 58; k++)
-                        {
-                                for (l = 48; l < 58; l++)
-                                {
-                                        if (i == j && j == k && k == l)
-                                        {
-                                                continue;
-                                        }
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			for (k = 48; k < 58; k++)
+			{
+				for (l = 48; l < 58; l++)
+				{
+					if (i == j && j == k && k == l)
+					{
+						continue;
+					}
 					if (i == k && j == l)
 					{
 						continue;
 					}
-                                        if (((j - 48) + ((i - 48) * 10)) >= ((l - 48) + ((k - 48) * 10)))
-                                        {
-                                                continue;
-                                        }
-                                        putchar(i);
-                                        putchar(j);
-                                        putchar(' ');
-                                        putchar(k);
-                                        putchar(l);
-					if(i == 57 && j == 56 && k == 57 && l == 57)
+					if (((j - 48) + ((i - 48) * 10)) >= ((l - 48) + ((k - 48) * 10)))
+					{
+						continue;
+					}
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(l);
+					if (i == 57 && j == 56 && k == 57 && l == 57)
 					{
 						break;
 					}
-                                        putchar(',');
-                                        putchar(' ');
-                                }
-                        }
-                }
-        }
-        putchar('\n');
-        return (0);
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
