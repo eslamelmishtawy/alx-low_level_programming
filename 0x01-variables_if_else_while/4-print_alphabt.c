da3fa2836959d6ce67fd1e73cc88e29b8d160e58
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-06-16 04:54:43 -0700

    ll

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char alpha[] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; i < 52; i++)
+	{
+		if (alpha[i] == 'q' || alpha[i] = 'e')
+		{
+			continue;
+		}
+		putchar(alpha[i]);
+	}
+	putchar('\n');
+	return (0);
+}
