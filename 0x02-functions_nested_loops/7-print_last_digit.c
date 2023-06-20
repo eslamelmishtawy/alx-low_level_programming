564db4865bc4bb4aca3c74bc1380b7bfff461e9c
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-06-20 07:34:08 -0700

    s

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_last_digit - prints all alphabets.
+ * @c: char under test
+ * Return: always 0
+ */
+
+int print_last_digit(int c)
+{
+	return (c % 10);
+}
