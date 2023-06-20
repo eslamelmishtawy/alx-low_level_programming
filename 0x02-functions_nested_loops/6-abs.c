ae67261a75fe6370d6d0e8c4be3f69222b8e758d
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-06-20 07:30:28 -0700

    s

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - prints all alphabets.
+ * @c: char under test
+ * Return: always 0
+ */
+
+int _abs(int c)
+{
+	return (abs(c))
+}
