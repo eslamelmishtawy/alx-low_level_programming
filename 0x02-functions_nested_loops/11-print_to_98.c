b27dfc6c629728caebd36b16b2c270347e81a517
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-06-20 14:27:36 -0700

    s

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,13 +16,27 @@ void print_to_98(int i)
 		for (i = n; i > 97; i--)
 		{
 			printf("%d", i);
+			if (i == 98)
+			{
+				putchar('\n');
+				break;
+			}
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	else
 	{
 		for (i = n; i < 99; i++)
 		{
-			printf("%d", i)
+			printf("%d", i);
+			if (i == 98)
+			{
+				putchar('\n');
+				break;
+			}
+			putchar(',');
+			putchar(' ');
 		}
 	}
 }
