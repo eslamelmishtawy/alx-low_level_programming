14d8785de5eaa55f4c8644735f7deebd551af0d0
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-06-20 10:58:53 -0700

    s

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * times_table - prints all alphabets.
+ *
+ * Return: always 0
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int x;
+	int y;
+
+	for (i = 48; i < 51; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			x = i - 48;
+			y = j - 48;
+			a = x * y;
+			a = a + 48
+			putchar(a);
+			if (y > 9)
+			{
+				putchar('\n')
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+}
