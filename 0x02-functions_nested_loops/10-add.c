6f91cf31f7e6a9f7647d130313ddef1083cf1e48
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-06-20 13:51:01 -0700

    s

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * add - prints all alphabets.
+ * @a: first number
+ * @b: second number
+ * Return: always 0
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
