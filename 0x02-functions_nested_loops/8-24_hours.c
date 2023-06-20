003529e4e506153ea32659ae1e0789ed64ee9bc1
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-06-20 10:34:58 -0700

    s

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * jack_bauer - prints all alphabets.
+ * @c: char under test
+ * Return: always 0
+ */
+
+void jack_bauer(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			for (k = 0; k < 10; k++)
+			{
+				for (l = 0; l < 10; l++)
+				{
+					_putchar(i);
+					_putchar(j);
+					_putchar(':');
+					_putchar(k);
+					_putchar(l);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
