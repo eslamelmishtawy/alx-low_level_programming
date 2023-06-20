d86421cb9779ac12bf43284ed4703a3b5b7f6617
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-06-20 13:37:38 -0700

    s

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,7 +23,7 @@ void times_table(void)
 			x = i - 48;
 			y = j - 48;
 			a = x * y;
-			if (a > 10)
+			if (a >= 10)
 			{
 				putchar((a / 10) + '0');
 				putchar((a % 10) + '0');
@@ -33,7 +33,6 @@ void times_table(void)
 					continue;
 				}
 				putchar(',');
-				putchar(' ');
 				
 			}
 			else
