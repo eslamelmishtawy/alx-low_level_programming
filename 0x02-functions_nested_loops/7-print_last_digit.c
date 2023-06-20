f59c6fdfb4b9867b46c18e8a02c37af89f2cd2cf
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-06-20 07:54:45 -0700

    s

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,10 +14,11 @@ int print_last_digit(int c)
 	int n;
 
 	n = (abs(c) % 10);
-	n = n + (n * 10);
 	if (c < 0)
 	{
+		_putchar(n + '0');
 		return (-1 * n);
 	}
+	_putchar (n + '0');
 	return (n);
 }
