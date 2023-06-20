df58cffb064bf0305c08058d4254c4f905da1435
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-06-20 14:29:39 -0700

    s

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,14 +4,14 @@
 
 /**
  * print_to_98 - prints all alphabets.
- * @i: first number
+ * @n: first number
  * Return: always 0
  */
 void print_to_98(int n)
 {
 	int i;
-	
-	if(n > 98)
+
+	if (n > 98)
 	{
 		for (i = n; i > 97; i--)
 		{
