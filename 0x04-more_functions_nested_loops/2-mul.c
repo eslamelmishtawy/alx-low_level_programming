a2e0e8dbabdff957d3fae39c59f173dc210c511c
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-06-22 06:28:49 -0700

    s

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _ismul - prints all alphabets.
+ * @a: char under test
+ * @c: char under test
+ * Return: always 0
+ */
+
+int _ismul(int a, int c)
+{
+	return (a * c);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,4 @@
 #include <stdio.h>
 int _isdigit(int c);
 int _isupper(int c);
+int mul(int a, int b);
