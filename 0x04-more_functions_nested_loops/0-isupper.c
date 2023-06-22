edcf6d59c215c1396aa205b5e3d0da062f3a6f1c
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-06-22 06:14:56 -0700

    s

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _islower - prints all alphabets.
+ * @c: char under test
+ * Return: always 0
+ */
+
+int _isupper(int c)
+{
+	if (isupper(c) > 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
