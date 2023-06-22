c36b6e9b8891b4cf067ed81b096543d05950dfd4
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-06-22 08:49:58 -0700

    s

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * print_diagonal - prints all alphabets.
+ * @n: number of line
+ * Return: always 0
+ */
+
+void print_diagonal(int n)
+{
+	int j;
+	int i;
+
+	for (j = 0; j < n; j++)
+	{
+		i = j + 1;
+		putchar('\\');
+		putchar('\n');
+		while (i--)
+		{
+			putchar(' ');
+		}
+	}
+	if (n == 0)
+	{
+		putchar('\n');
+	}
+}
