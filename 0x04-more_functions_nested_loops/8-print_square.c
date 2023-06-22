f8f3d1df306648614592c24a0e177044590e98fd
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-06-22 12:56:18 -0700

    s

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * print_square - prints all alphabets.
+ * @n: number of line
+ * Return: always 0
+ */
+
+void print_square(int n)
+{
+	int j;
+	int i;
+
+	for (j = 0; j < n; j++)
+	{
+		for (i = 0; i < n; i++)
+		{
+			putchar('#');
+		}
+		putchar('\n');
+	}
+}
