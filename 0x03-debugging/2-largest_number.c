9b5b9d9cbeb7a0c7cd9126fdb281a6b07b89f1c3
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-06-22 04:47:46 -0700

    s

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,10 +11,10 @@
 int largest_number(int a, int b, int c)
 {
 	int largest;
-	
+
 	if (a > b && a > c)
 	{
-	    	largest = a;
+		largest = a;
 	}
 	else if (b > a && b > c)
 	{
@@ -24,6 +24,6 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
-	
+
 	return (largest);
 }
