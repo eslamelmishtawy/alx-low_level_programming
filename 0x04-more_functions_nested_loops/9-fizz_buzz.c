2cc37bc7d5e8174fa1a21d08533595f53ea38c03
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-06-22 13:13:21 -0700

    s

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * main - prints all alphabets.
+ *
+ * Return: always 0
+ */
+
+void main(void)
+{
+	int j;
+
+	for (j = 1; j < 101; j++)
+	{
+		if (j % 3 == 0 && j % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (j % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (j % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", j);
+		}
+		if (j == 100)
+		{
+			break;
+		}
+		putchar(' ');
+	}
+}
