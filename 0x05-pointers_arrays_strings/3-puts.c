b2d408acb7d5f4237d00caeafd3553eac9da8289
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-06-26 04:36:19 -0700

    s

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - reset value on n to 98
+ * @a: pointer to value of n
+ *
+ * Return: return n
+ */
+int _puts(char *a)
+{
+	int i;
+
+	i = 0;
+	while (a[i] != '\0')
+	{
+		putchar(a[i] + '0');
+	}
+	putchar('\n');
+}
