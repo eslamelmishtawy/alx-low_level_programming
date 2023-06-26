12fbe8809dfb46dee518e05139acf7f762ab8c2c
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-06-26 05:11:23 -0700

    s

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - reset value on n to 98
+ * @a: pointer to value of n
+ *
+ * Return: return n
+ */
+void print_rev(char *a)
+{
+	int i;
+
+	i = strlen(*a);
+	while (a[i] != '\0')
+	{
+		putchar(a[i]);
+		i--;
+	}
+	putchar('\n');
+}
