366d35955e98c3306e876fa966ce110e194197f1
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-06-26 13:05:25 -0700

    s

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,12 +11,20 @@ void rev_string(char *a)
 	int i;
 	int len;
 	char tmp;
+	int len1;
 
-	len = strlen(a) - 1;
+	len1 = 0;
+
+	while (a[len] != '\0')
+	{
+		len++;
+	}
+
+	len1 = len - 1;
 	for (i = 0; i < len / 2; i++)
 	{
 		tmp = a[i];
-		a[i] = a[len];
-		a[len--] = tmp;
+		a[i] = a[len1];
+		a[len1--] = tmp;
 	}
 }
