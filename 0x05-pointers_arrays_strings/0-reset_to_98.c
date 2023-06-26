9c1bfbeb0f812d6a79ce71ca1dbdfba803f0522a
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-06-26 03:40:20 -0700

    s

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset value on n to 98
+ * @*n: pointer to value of n
+ *
+ * Retrun: return n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+	return *n;
+}
