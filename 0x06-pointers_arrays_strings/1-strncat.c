1673a777cbe1e61165ba9718dc9559a670c2b257
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-06-29 15:35:03 -0700

    s

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,10 +4,10 @@
  * _strncat - reset value on n to 98
  * @a: pointer to value of n
  * @b: c
- *
+ * @n: n
  * Return: return n
  */
-char *_strncat(char *a, char *b)
+char *_strncat(char *a, char *b, int n)
 {
-	return (strncat(a, b));
+	return (strncat(a, b, n));
 }
