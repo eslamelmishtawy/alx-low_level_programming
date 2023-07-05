79cf229a10b5f7ef7c3d51a38bbe8c336a8e6cb5
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-07-05 10:16:44 -0700

    s

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+int t(int i, int j)
+{
+	if (i - j == 0)
+	{
+		return (0);
+	}
+	return (t(i - j, j + 2) + 1);
+}
+
+/**
+ * _sqrt_recursion - check the code
+ * @n: s
+ * 
+ * Return: Always 0.
+ */
+int _sqrt_recursion(int n)
+{
+	t(n, 1);
+}
