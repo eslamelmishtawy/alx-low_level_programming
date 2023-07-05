1328aef91998da92d972f66331d17e32ba2ce779
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-07-05 08:25:10 -0700

    s

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - check the code
+ * @s: s
+ *
+ * Return: Always 0.
+ */
+void _puts_recursion(char *s)
+{
+    while(*s != '\0'):
+	{
+	       	puts(s[0]);
+		_put_recursion(s+1);
+	}
+    puts('\n');
+}
