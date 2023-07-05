3c73790647bac6813e2f903cee9df08013d1e029
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-07-05 09:17:11 -0700

    s

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _print_rev_recursion - check the code
+ * @s: s
+ *
+ * Return: Always 0.
+ */
+void _print_rev_recursion(char *s)
+{
+	
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+	}
+	_putchar(*s);
+	
+}
