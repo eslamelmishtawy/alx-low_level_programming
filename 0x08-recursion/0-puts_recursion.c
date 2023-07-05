c144af27e6250e47e9b015a2e8e9e83f7fafd19f
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-07-05 08:33:01 -0700

    s

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * _puts_recursion - check the code
  * @s: s
@@ -10,7 +11,7 @@ void _puts_recursion(char *s)
 {
     while(*s != '\0')
 	{
-	       	puts(s[0]);
+	       	puts(*s);
 		_put_recursion(s+1);
 	}
     puts('\n');
