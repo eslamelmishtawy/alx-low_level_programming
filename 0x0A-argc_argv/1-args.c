99da9c910180093ad8942b348280aa2d3b336436
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-07-07 15:02:08 -0700

    s

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - main
+ * @argc: argc
+ * @argv: argv
+ * Return: absolute value of @n
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
