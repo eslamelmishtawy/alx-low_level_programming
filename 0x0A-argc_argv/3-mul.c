4ffd32333dbafb0b789ae71447ecc6e926419cd2
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-07-07 15:12:27 -0700

    s

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - main
  * @argc: argc
@@ -7,11 +8,15 @@
  */
 int main(int argc, char *argv[])
 {
+	int x;
+
 	if (argc < 1)
 	{
-		printf("Error");
+		printf("Error\n");
 		return (1);
 	}
-	return (argv[0] * argv[1]);
+	x = atoi(argv[1]) * atoi(argv[1]);
+	printf("%d\n",x);
+	return (0);
 
 }
