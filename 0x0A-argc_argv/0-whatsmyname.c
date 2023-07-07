5c5785f9e57ac9d5b5d93cb882a3fa331c5ac317
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-07-07 14:59:24 -0700

    s

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,8 +5,8 @@
  * @argv: argv
  * Return: absolute value of @n
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", argv[argc-argc]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
