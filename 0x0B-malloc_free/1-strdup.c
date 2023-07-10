4e00ea7097aaa6171575af5247a59006a801ab86
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-07-10 16:37:42 -0700

    s

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,18 +3,29 @@
 /**
  * main - main
  * @str: argc
- * 
+ *
  * Return: absolute value of @n
  */
 char *_strdup(char *str)
 {
 	char *ar;
+	int i;
 
 	if (str == NULL)
 	{
 		return (NULL);
 	}
 	ar = malloc(sizeof(char) * strlen(str));
+
+	if (ar == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < strlen(str); i++)
+	{
+		ar[i] == str[i]
+	}
 
 	return (ar);
 }
