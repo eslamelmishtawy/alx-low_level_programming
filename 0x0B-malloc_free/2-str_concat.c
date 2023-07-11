251aafb3d45101ea02c0c0aa0cb1164a75aecbb7
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-07-11 08:09:07 -0700

    s

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - main
+ * @str: argc
+ *
+ * Return: absolute value of @n
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *ar;
+	int i;
+	int s1_len = 0;
+	int s2_len = 0;
+
+	if (s1 == NULL)
+	{
+		*s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		*s2 = "";
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		s1_len++;
+	}
+	for (i = 0; s2[i] != '\0'; i++)
+	{
+		s2_len++;
+	}
+	ar = malloc(sizeof(char) * (s1_len + s2_len));
+
+	if (ar == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < s1_len; i++)
+	{
+		ar[i] = s1[i];
+	}
+	for (i = 0; i < s2_len; i++)
+	{
+		ar[s1_len + i] = s2[i];
+	}
+	ar[s1_len + s2_len] = '\0';
+
+	return (ar);
+}
