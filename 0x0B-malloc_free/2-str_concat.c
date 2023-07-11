4662931d05e1ffd5912d65b594d8021b1a24398d
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-07-11 08:16:34 -0700

    s

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,11 +15,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		*s1 = '\0';
+		s1 = '\0';
 	}
 	if (s2 == NULL)
 	{
-		*s2 = '\0';
+		s2 = '\0';
 	}
 
 	for (i = 0; s1[i] != '\0'; i++)
@@ -30,7 +30,7 @@ char *str_concat(char *s1, char *s2)
 	{
 		s2_len++;
 	}
-	ar = malloc(sizeof(char) * (s1_len + s2_len));
+	ar = malloc(sizeof(char) * (s1_len + s2_len + 1));
 
 	if (ar == NULL)
 	{
