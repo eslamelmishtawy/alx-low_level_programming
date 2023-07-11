afc0ce4eca39a1e1aba5ad4585a9b2467e7e4b88
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-07-11 08:41:14 -0700

    s

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,13 +13,13 @@ char *str_concat(char *s1, char *s2)
 	int s1_len = 0;
 	int s2_len = 0;
 
-	if (*s1 == NULL)
+	if (s1 == NULL)
 	{
-		*s1 = '\0';
+		s1 = "";
 	}
-	if (*s2 == NULL)
+	if (s2 == NULL)
 	{
-		*s2 = '\0';
+		s2 = "";
 	}
 
 	for (i = 0; s1[i] != '\0'; i++)
--- 0x0B-malloc_free/main.c
+++ 0x0B-malloc_free/main.c
@@ -8,33 +8,47 @@
  * Return: Always 0.
  */
 
-char *_strdup(char *str)
+char *str_concat(char *s1, char *s2)
 {
         char *ar;
         int i;
-	int len = 0;
-
-        if (str == NULL)
-        {
-                return (NULL);
-        }
-	for (i = 0; str[i] != '\0'; i++)
+        int s1_len = 0;
+        int s2_len = 0;
+	
+	printf("%s\n%s\n", s1,s2);
+        if (s1 == NULL)
 	{
-		len++;
+		s1 = "";
 	}
-	printf("%d\n", len);
-        ar = malloc(sizeof(char) * len);
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+        for (i = 0; s1[i] != '\0'; i++)
+        {
+                s1_len++;
+        }
+        for (i = 0; s2[i] != '\0'; i++)
+        {
+                s2_len++;
+        }
+        ar = malloc(sizeof(char) * (s1_len + s2_len + 1));
 
         if (ar == NULL)
         {
                 return (NULL);
         }
 
-        for (i = 0; i < len; i++)
+        for (i = 0; i < s1_len; i++)
         {
-                ar[i] = str[i];
+                ar[i] = s1[i];
         }
-	ar[len] = '\0';
+        for (i = 0; i < s2_len; i++)
+        {
+                ar[s1_len + i] = s2[i];
+        }
+        ar[s1_len + s2_len] = '\0';
 
         return (ar);
 }
@@ -42,10 +56,10 @@ int main(void)
 {
     char *s;
 
-    s = _strdup("ALX SE");
+    s = str_concat(NULL, "Holberton");
     if (s == NULL)
     {
-        printf("failed to allocate memory\n");
+        printf("failed\n");
         return (1);
     }
     printf("%s\n", s);
