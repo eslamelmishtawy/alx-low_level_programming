2151a3717bd935f6c4f76bb5e3933cb9d0dee0fe
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-07-11 07:35:09 -0700

    s

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * create_array - main
+ * @s: s
+ * @v: v
+ * Return: absolute value of @n
+ */
+char *create_array(unsigned int s, char v)
+{
+	char *ar;
+
+	ar = malloc(sizeof(char) * size);
+	if (size == 0 || ar = NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		ar[i] = v;
+	}
+
+	return (ar);
+}
