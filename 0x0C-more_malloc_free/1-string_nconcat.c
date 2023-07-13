925c490ceb79cabae6c52d76b0c12b919c145dcb
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-07-13 14:10:56 -0700

    s

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,43 +16,26 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int s2_len = 0;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
-
-        for (i = 0; s1[i] != '\0'; i++)
-        {
-                s1_len++;
-        }
-        for (i = 0; s2[i] != '\0'; i++)
-        {
-                s2_len++;
-        }
-	if (n >= s2_len )
-	{
-        	ar = malloc(sizeof(char) * (s1_len + s2_len + 1));
-	}
+	for (i = 0; s1[i] != '\0'; i++)
+		s1_len++;
+	for (i = 0; s2[i] != '\0'; i++)
+		s2_len++;
+	if (n >= s2_len)
+		ar = malloc(sizeof(char) * (s1_len + s2_len + 1));
 	else
 	{
 		s2_len = n;
 		ar = malloc(sizeof(char) * (s1_len + n + 1));
 	}
 	if (ar == NULL)
-	{
 		return (NULL);
-	}
 	for (i = 0; i < s1_len; i++)
-	{
 		ar[i] = s1[i];
-	}
 	for (i = 0; i < s2_len; i++)
-	{
 		ar[s1_len + i] = s2[i];
-	}
 	ar[s1_len + s2_len] = '\0';
 
 	return (ar);
