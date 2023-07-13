b86498f97b42b5586e655437239ca04742e8408b
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-07-13 14:31:47 -0700

    s

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _calloc - main
+ * @b: s
+ *
+ * Return: absolute value of @n
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ar;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	ar = malloc(size * nmemb);
+	if (ar == NULL)
+	{
+		return (NULL);
+	}
+	return (ar);
+}
