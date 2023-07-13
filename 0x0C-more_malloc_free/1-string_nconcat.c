645b39872b409f80e25ad97d8c011a53a202ccfd
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-07-13 14:04:31 -0700

    s

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,4 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
+#include "main.h"
 
 /**
  * string_nconcat - main
--- 0x0C-more_malloc_free/main.c
+++ 0x0C-more_malloc_free/main.c
@@ -1,68 +1,18 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * main - check the code for ALX School students.
+ * main - check the code
  *
  * Return: Always 0.
  */
-
-char *str_concat(char *s1, char *s2)
-{
-        char *ar;
-        int i;
-        int s1_len = 0;
-        int s2_len = 0;
-	
-	printf("%s\n%s\n", s1,s2);
-        if (s1 == NULL)
-	{
-		s1 = "";
-	}
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
-
-        for (i = 0; s1[i] != '\0'; i++)
-        {
-                s1_len++;
-        }
-        for (i = 0; s2[i] != '\0'; i++)
-        {
-                s2_len++;
-        }
-        ar = malloc(sizeof(char) * (s1_len + s2_len + 1));
-
-        if (ar == NULL)
-        {
-                return (NULL);
-        }
-
-        for (i = 0; i < s1_len; i++)
-        {
-                ar[i] = s1[i];
-        }
-        for (i = 0; i < s2_len; i++)
-        {
-                ar[s1_len + i] = s2[i];
-        }
-        ar[s1_len + s2_len] = '\0';
-
-        return (ar);
-}
 int main(void)
 {
-    char *s;
+    char *concat;
 
-    s = str_concat(NULL, "Holberton");
-    if (s == NULL)
-    {
-        printf("failed\n");
-        return (1);
-    }
-    printf("%s\n", s);
-    free(s);
+    concat = string_nconcat("Best ", "School !!!", 6);
+    printf("%s\n", concat);
+    free(concat);
     return (0);
 }
