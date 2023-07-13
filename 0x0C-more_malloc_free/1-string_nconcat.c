a5d823c49015f1a4470c32d3c36d696be51bd7eb
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-07-13 13:55:34 -0700

    s

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -58,3 +58,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	return (ar);
 }
+int main(void)
+{
+    char *concat;
+
+    concat = string_nconcat("Best ", "School !!!", 6);
+    printf("%s\n", concat);
+    free(concat);
+    return (0);
+}
