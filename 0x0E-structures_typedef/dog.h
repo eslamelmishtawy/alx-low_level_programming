3a86cda3cbb9c38792f83cd18e6026156631f61b
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-07-18 10:21:40 -0700

    ss

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,19 @@
-typedef struct dog;
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
 struct dog {
 	char *name;
 	float age;
 	char *owner;
 };
+
+typedef struct dog d;
+#endif
