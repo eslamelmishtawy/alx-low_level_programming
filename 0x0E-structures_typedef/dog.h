acb6de6a56134c50051a9566f997a0586a914cbb
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-07-18 15:14:48 -0700

    ss

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,7 +16,7 @@ struct dog
 	char *owner;
 };
 
-typedef struct dog d;
+typedef struct dog dog_t;
 
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
