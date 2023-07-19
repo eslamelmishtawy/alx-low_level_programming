e4d21db889fc64aeda6a59d835b46583d74e173c
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-07-19 12:43:57 -0700

    ss

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - prints a name as is
+ * @array: name of the person
+ * @size: function
+ * @action: function
+ *
+ * Return: none
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
