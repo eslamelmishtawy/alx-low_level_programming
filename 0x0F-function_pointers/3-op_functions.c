eb252da362f2ac9eb7e93e4b646ef7da2a4989ef
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-07-19 15:46:27 -0700

    s

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+/**
+ * op_add - add function
+ * @a: number
+ * @b: number
+ *
+ * Return: Sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - sub function
+ * @a: number
+ * @b: number
+ *
+ * Return: Sum
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - mul function
+ * @a: number
+ * @b: number
+ *
+ * Return: Sum
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - div function
+ * @a: number
+ * @b: number
+ *
+ * Return: Sum
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - mod function
+ * @a: number
+ * @b: number
+ *
+ * Return: Sum
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
