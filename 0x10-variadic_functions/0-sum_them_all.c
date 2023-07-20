6d6ea55b8fc077b7a13849315cfe7bf2e1dc62ca
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-07-20 14:41:10 -0700

    s

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -20,7 +20,7 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(ptr, n);
 	for (i = 0; i < n - 1; i++)
 	{
-		sum += va_arg(ptr, unsigned int);
+		sum += va_arg(ptr, int);
 	}
 	va_end(ptr);
 	return (sum);
