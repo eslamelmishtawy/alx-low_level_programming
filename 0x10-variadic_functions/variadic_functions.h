ac0fffaa810ea4458d5e5d1dd8d0449cb1abbd03
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-07-20 14:47:34 -0700

    s

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,5 +4,8 @@
 int sum_them_all(const unsigned int n, ...);
 #include <stdarg.h>
 #include <stdio.h>
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
