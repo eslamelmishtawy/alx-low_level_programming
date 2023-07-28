dbf3a917e8430a8948931d2515aa4c3baf3cc64c
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-07-28 16:29:30 -0700

    s

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * add_node - check the code
+ * @head: h
+ * @str: s
+ * Return: Always 0.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	int len = 0;
+
+	if (str == NULL)
+		return (NULL);
+	new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->str = strdup(str);
+
+	while (str)
+	{
+		len++;
+	}
+	new->len = len;
+	new->next = NULL;
+	*head = new;
+
+	return (new);
+}
