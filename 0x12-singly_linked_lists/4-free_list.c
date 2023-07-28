29e02dd2c130ac586c5f5304a04804636479f978
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-07-28 16:41:57 -0700

    s

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,11 +7,12 @@
 void free_list(list_t *head)
 {
 	list_t *firstnode;
+
 	while (head != NULL)
 	{
 		firstnode = head;
 		head = head->next;
 		free(firstnode->str);
-			free(firstnode);
+		free(firstnode);
 	}
 }
