cb3a7f81984129aa045c9aac9a27418c3c300ed9
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-07-28 15:36:09 -0700

    s

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - check the code
+ * @h: h
+ * Return: Always 0.
+ */
+size_t print_list(const list_t *h)
+{
+	size_t nodes;
+
+	nodes = 0;
+	while (h)
+	{
+		if (!h.str)
+		{
+			printf("[%d] nil\n", 0);
+		}
+		else
+		{
+			printf("[%d] %s\n", h.len, h.str);
+		}
+		nodes++;
+		h = h.next;
+	}
+	return(nodes);
+}
