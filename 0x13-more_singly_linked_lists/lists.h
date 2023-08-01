ed49cb0536a4e0b21b836a16e39f296da61d981f
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-08-01 03:51:28 -0700

    s

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -18,14 +18,10 @@
 typedef struct listint_s
 {
 	int n;
-	struct listnt_s *next;
+	struct listint_s *next;
 } listint_t;
 
-size_t print_listint(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
+size_t print_listint(const listint_t *h);
 
 #endif
 
