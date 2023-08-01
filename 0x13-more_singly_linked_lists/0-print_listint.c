fb0bee77300ac0916b4cadc4154fecf2a6030f75
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-08-01 03:43:23 -0700

    s

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_listint - check the code
+ * @h: h
+ * Return: Always 0.
+ */
+size_t print_listint(const list_t *h)
+{
+	size_t nodes;
+
+	nodes = 0;
+	while (h)
+	{
+		if (!h->n)
+		{
+			printf("[%d]\n", 0);
+		}
+		else
+		{
+			printf("[%d]\n",  h->n);
+		}
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
