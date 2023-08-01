147f28164ea1e2e7897601bfd8a4347365783ee1
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-08-01 07:38:10 -0700

    ss

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - check the code
+ * @head: h
+ * Return: Always 0.
+ */
+void free_listint(listint_t *head)
+{
+
+	listint_t *firstnode;
+	while (!head)
+	{
+		firstnode = head;
+		head = head->next;
+		free(firstnode);
+	}
+}
