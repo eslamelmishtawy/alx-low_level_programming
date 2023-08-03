26acb70ebf0914be088accf3fecf97da605e70ef
eslamelmishtawy <eslamelmishtawy@gmail.com>
2023-08-03 16:34:51 -0700

    ss

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,18 +8,23 @@
  */
 void print_binary(unsigned long int n)
 {
+	int flag = 0;
+	unsigned long int mask = 1;
+
+	mask <<= 63;
 	if (n == 0)
 		_putchar('0');
-	while (n != 0)
+
+	while (mask > 0)
 	{
-		if (n % 2 == 0)
-		{
+		if ((n & mask) == 0 && flag == 1)
 			_putchar('0');
-		}
-		else
+		if ((n & mask) != 0)
 		{
 			_putchar('1');
+			flag = 1;
 		}
-		n = n / 2;
+
+		mask = mask >> 1;
 	}
 }
