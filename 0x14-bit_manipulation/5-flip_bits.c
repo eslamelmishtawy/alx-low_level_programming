e985ddeda93634e4909f83c5cc642aecfc275a12
root <eslamelmishtawy@gmail.com>
2023-09-28 07:57:03 -0700

    s

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -6,10 +6,10 @@
  * @m: index
  * Return: Always 0.
  */
-int flip_bits(unsigned long int n, unsigned long int m)
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int x = n ^ m;
-	int counter = 0;
+	unsigned int counter = 0;
 
 	while (x)
 	{
