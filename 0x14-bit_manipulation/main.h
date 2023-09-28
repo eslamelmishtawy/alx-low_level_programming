2624aa950612305c3cf2f9f007cd256ee34c817b
root <eslamelmishtawy@gmail.com>
2023-09-28 06:58:07 -0700

    s

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,4 +7,5 @@ int get_bit(unsigned long int n, unsigned int index);
 unsigned int binary_to_uint(const char *b);
 unsigned int power(unsigned int counter);
 void print_binary(unsigned long int n);
+int set_bit(unsigned long int *n, unsigned int index);
 #endif
