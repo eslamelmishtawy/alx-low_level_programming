ef2501fd08a9064229f60a646dc90dd7ea99f916
root <eslamelmishtawy@gmail.com>
2023-09-28 07:02:23 -0700

    s

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * clear_bit - check the code
+ * @n: decimal number
+ * @index: index
+ * Return: Always 0.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > sizeof(unsigned long int) * 8)
+		return (-1);
+	*n = *n & ~(1 << index);
+	return (1);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,4 +8,5 @@ unsigned int binary_to_uint(const char *b);
 unsigned int power(unsigned int counter);
 void print_binary(unsigned long int n);
 int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
 #endif
