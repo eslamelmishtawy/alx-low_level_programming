6faf1de6458138fad1381dc4eff7032aa57f7a7b
root <eslamelmishtawy@gmail.com>
2023-09-28 13:36:06 -0700

    s

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * flip_bits - check the code
+ * @n: decimal number
+ * @m: index
+ * Return: Always 0.
+ */
+int get_endianness(void);
+{
+	int x = 1;
+	char *c;
+
+	c = &1;
+	return ((int)*c);
+
+}
