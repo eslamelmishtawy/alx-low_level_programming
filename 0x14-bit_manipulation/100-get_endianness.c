9b6330de55fc77b8ce916cc14b6d45795b596a71
root <eslamelmishtawy@gmail.com>
2023-09-28 13:38:48 -0700

    s

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -4,12 +4,12 @@
  * get_endianness - check the code
  * Return: Always 0.
  */
-int get_endianness(void);
+int get_endianness(void)
 {
-	int x = 1;
+	char x = 1;
 	char *c;
 
-	c = &1;
+	c = &x;
 	return ((int)*c);
 
 }
