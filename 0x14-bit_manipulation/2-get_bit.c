8047753eafc75c5ef5bc85be8e2f9345b6c9a204
root <eslamelmishtawy@gmail.com>
2023-09-28 05:10:39 -0700

    s

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * print_binary - check the code
+ * get_bit - check the code
  * @n: decimal number
+ * @index: index
  * Return: Always 0.
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	int l;
 
-	if (index > n)
+	if (index >= n)
 	{
 		return (-1);
 	}
