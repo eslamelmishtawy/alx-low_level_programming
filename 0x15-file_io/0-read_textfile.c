41d8b72c485abdfe214efe4e4c2e540cb568165c
root <eslamelmishtawy@gmail.com>
2023-10-02 05:59:05 -0700

    s

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * read_textfile - check the code
+ * @filename: f
+ * letters: l
+ * Return: Always 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buffer;
+	ssize_t rd, wr;
+
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	rd = read(fd, buffer, letters);
+	wr = write(STDOUT_FILENO, buffer, rd);
+	close(fd);
+	free(buffer);
+}
