8dbc1f6940196c63f0ff0dba3d08bfd718398336
root <eslamelmishtawy@gmail.com>
2023-10-02 06:30:39 -0700

    s

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * create_file - check the code
+ * @filename: f
+ * @text_content: l
+ * Return: Always 0.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	int counter;
+	ssize_t rdwr;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (!text_content)
+		return (1);
+
+	for (counter = 0; text_content[counter]; counter++)
+		;
+	rdwr = write(fd, text_content, counter);
+	if (rdwr == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
