ad11266c9a93241113757497bba7ffc9064bffdb
root <eslamelmishtawy@gmail.com>
2023-11-09 13:10:17 -0800

    ss

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - check the code
+ * @head: h
+ * Return: Always EXIT_SUCCESS.
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head != NULL)
+	{
+		sum = sum + head->n;
+		head = head->next
+	}
+	return (sum);
+}
