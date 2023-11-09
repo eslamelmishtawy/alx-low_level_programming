a33b77fc8fc69468655dc0067a67760d201b4d15
root <eslamelmishtawy@gmail.com>
2023-11-09 12:30:34 -0800

    ss

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -14,9 +14,6 @@
  * @n: integer
  * @prev: points to the previous node
  * @next: points to the next node
- *
- * Description: doubly linked list node structure
- * 
  */
 typedef struct dlistint_s
 {
