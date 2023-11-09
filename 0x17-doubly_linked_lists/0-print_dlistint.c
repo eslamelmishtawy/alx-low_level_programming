b53673833fe7db764d7d3ff2277775008a13c779
root <eslamelmishtawy@gmail.com>
2023-11-09 12:27:22 -0800

    ss

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,12 +2,13 @@
 
 /**
  * print_dlistint - check the code
- * @dlistint_t: h
+ * @h: h
  * Return: Always EXIT_SUCCESS.
  */
-size_t print_dlistint(const dlistint_t *h);
+size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i;
+
 	while (h)
 	{
 		i++;
