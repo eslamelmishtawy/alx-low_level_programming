54ec5eb8f14c33c9c0f0ed16723c83fcca94ea95
root <eslamelmishtawy@gmail.com>
2023-12-01 14:14:39 -0800

    sz

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,9 +1,10 @@
-import "hash_tables.h"
+#include "hash_tables.h"
+
 /**
- * hash_djb2 - implementation of the djb2 algorithm
- * @str: string used to generate hash value
+ * hash_djb2 - hash function to implement djb2 algorithm.
+ * @str: pointer to null-terminated byte string to be examined.
  *
- * Return: hash value
+ * Return: The new hash.
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
