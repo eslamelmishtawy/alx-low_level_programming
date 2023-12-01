254702484852f10f9aae65017f1a13f48b93ac55
root <eslamelmishtawy@gmail.com>
2023-12-01 15:37:33 -0800

    ss

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - table
+ * @ht: table
+ * @key: key
+ *
+ * Return: element
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsinged long int index = 0;
+	hash_node_t *node;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
+	index = key_index((const unsinged char *)key, ht->size);
+	node = ht->array[index];
+
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+	}
+	return (NULL);
+}
