1f1c1352b865767f0a8d4fcb49ab660d2d4eb8f1
root <eslamelmishtawy@gmail.com>
2023-12-01 15:41:26 -0800

    ss

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,13 +9,13 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsinged long int index = 0;
+	unsigned long int index = 0;
 	hash_node_t *node;
 
 	if (ht == NULL || key == NULL)
 		return (NULL);
 
-	index = key_index((const unsinged char *)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 	node = ht->array[index];
 
 	while (node != NULL)
