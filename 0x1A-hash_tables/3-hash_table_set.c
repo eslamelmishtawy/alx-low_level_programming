74edd8ad1fd8a26d2a8bc0d4f492b79492f56506
root <eslamelmishtawy@gmail.com>
2023-12-01 15:26:19 -0800

    ss

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,4 +1,4 @@
-include "hash_tables.h"
+#include "hash_tables.h"
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
@@ -33,7 +33,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 }
 
-hash_node_t *create_item(char *key, char *value)
+hash_node_t *create_item(const char *key, const char *value)
 {
 	hash_node_t *new_item;
 
