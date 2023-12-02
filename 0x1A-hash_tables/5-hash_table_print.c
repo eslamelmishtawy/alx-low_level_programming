27d55ac64ac91d95d5163d30e77cf2504ff122a3
root <eslamelmishtawy@gmail.com>
2023-12-01 16:29:30 -0800

    ss

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -22,7 +22,7 @@ void hash_table_print(const hash_table_t *ht)
 				if (n != 0)
 					printf(", ");
 				printf("'%s': '%s'", node->key, node->value);
-			       	n++;
+				n++;
 				node = node->next;
 			}
 		}
